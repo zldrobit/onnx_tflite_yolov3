c930d7fe95e66218d93e356087549a2c0894f007
zldrobit <zldrobit@126.com>
2020-06-19 17:18:02 +0800

    TFLite OpenCL and OpenGLES GPU Delegate on Linux

--- tflite_detect.py
+++ tflite_detect.py
@@ -6,16 +6,15 @@ from models import *  # set ONNX_EXPORT in models.py
 from utils.datasets import *
 from utils.utils import *
 
-import onnxruntime as rt
 import cv2
 import time
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.lite.python.interpreter import load_delegate
 
 
 def detect(save_txt=False, save_img=False):
-    # img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
     img_size = (416, 416)
     out, source, weights, half, view_img = opt.output, opt.source, opt.weights, opt.half, opt.view_img
     webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
@@ -26,25 +25,16 @@ def detect(save_txt=False, save_img=False):
         shutil.rmtree(out)  # delete output folder
     os.makedirs(out)  # make new output folder
 
-    # Initialize model
-    # model = Darknet(opt.cfg, img_size)
-    # sess = rt.InferenceSession("weights/export.onnx")
-    # input_name = sess.get_inputs()[0].name
-
     # Load TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path=opt.weights)
+    if opt.delegate:
+        interpreter = tf.lite.Interpreter(model_path=opt.weights, experimental_delegates=[load_delegate(opt.delegate)])
+    else:
+        interpreter = tf.lite.Interpreter(model_path=opt.weights)
     interpreter.allocate_tensors()
 
     # Get input and output tensors.
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
-
-    # Load weights
-    # attempt_download(weights)
-    # if weights.endswith('.pt'):  # pytorch format
-    #     model.load_state_dict(torch.load(weights, map_location=device)['model'])
-    # else:  # darknet format
-    #     _ = load_darknet_weights(model, weights)
 
     # Second-stage classifier
     classify = False
@@ -52,25 +42,6 @@ def detect(save_txt=False, save_img=False):
         modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
         modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
         modelc.to(device).eval()
-
-    # Fuse Conv2d + BatchNorm2d layers
-    # model.fuse()
-
-    # Eval mode
-    # model.to(device).eval()
-
-    # Export mode
-    # if ONNX_EXPORT:
-    #     img = torch.zeros((1, 3) + img_size)  # (1, 3, 320, 192)
-    #     torch.onnx.export(model, img, 'weights/export.onnx', verbose=False, opset_version=10)
-    #     # torch.onnx.export(model, img, 'weights/export.onnx', verbose=False, opset_version=11)
-
-    #     # Validate exported model
-    #     import onnx
-    #     model = onnx.load('weights/export.onnx')  # Load the ONNX model
-    #     onnx.checker.check_model(model)  # Check that the IR is well formed
-    #     print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
-    #     return
 
     # Half precision
     half = half and device.type != 'cpu'  # half precision only supported on CUDA
@@ -91,6 +62,12 @@ def detect(save_txt=False, save_img=False):
     classes = load_classes(parse_data_cfg(opt.data)['names'])
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(classes))]
 
+    OUTPUT_WIDTH = [52, 26, 13]
+    NUM_BOXES_PER_BLOCK = 3
+    INPUT_SIZE = 416 
+    MASKS = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    ANCHORS = [10,13,  16,30,  33,23,  30,61,  62,45,  59,119,  116,90,  156,198,  373,326]
+
     # Run inference
     t0 = time.time()
     for path, img, im0s, vid_cap in dataset:
@@ -98,7 +75,7 @@ def detect(save_txt=False, save_img=False):
 
         img0 = img
         start = time.time()
-        for i in range(10):
+        for i in range(1):
             # Get detections
             # img = torch.from_numpy(img).to(device)
             # if img.ndimension() == 3:
@@ -109,12 +86,45 @@ def detect(save_txt=False, save_img=False):
 
             img = img0
             img = img[None, :, :, :]
+            img = np.transpose(img, [0, 2, 3, 1])
             input_data = img.astype(np.float32)
+            print('input_data.shape', input_data.shape)
             interpreter.set_tensor(input_details[0]['index'], input_data)
             interpreter.invoke()
-            output_data = interpreter.get_tensor(output_details[0]['index'])
-            print("output_data.shape", output_data.shape)
-            pred = torch.Tensor(output_data)
+            out_ = []
+            output_data0 = torch.Tensor(interpreter.get_tensor(output_details[0]['index']))
+            output_data1 = torch.Tensor(interpreter.get_tensor(output_details[1]['index']))
+            output_data2 = torch.Tensor(interpreter.get_tensor(output_details[2]['index']))
+            out_.extend([output_data0[0], output_data1[0], output_data2[0]])
+            output_data = torch.cat((
+                output_data0.view(-1, 85),
+                output_data1.view(-1, 85),
+                output_data2.view(-1, 85)), 0)
+
+            print('len(out)', len(out))
+
+            for i in range(len(OUTPUT_WIDTH)):
+                grid_width = OUTPUT_WIDTH[i]
+                for y in range(grid_width):
+                    for x in range(grid_width):
+                        for b in range(NUM_BOXES_PER_BLOCK):
+                            xPos = (x + torch.sigmoid(out_[i][y][x][b][0])) * (INPUT_SIZE / grid_width);
+                            yPos = (y + torch.sigmoid(out_[i][y][x][b][1])) * (INPUT_SIZE / grid_width);
+                            w = (torch.exp(out_[i][y][x][b][2]) * ANCHORS[2 * MASKS[i][b]]);
+                            h = (torch.exp(out_[i][y][x][b][3]) * ANCHORS[2 * MASKS[i][b] + 1]);
+                            out_[i][y][x][b][0] = xPos
+                            out_[i][y][x][b][1] = yPos
+                            out_[i][y][x][b][2] = w
+                            out_[i][y][x][b][3] = h
+            
+            # print("output_data.shape", output_data.shape)
+            # pred = torch.Tensor(output_data)
+            pred = torch.cat((
+                out_[0].view(-1, 85),
+                out_[1].view(-1, 85),
+                out_[2].view(-1, 85)), 0)
+            pred = pred[None, ...]
+            print('pred.shape', pred.shape)
             # pred = torch.Tensor(pred)
             if opt.half:
                 pred = pred.float()
@@ -141,7 +151,8 @@ def detect(save_txt=False, save_img=False):
             # print("len(det)", len(det))
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                # det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(img.shape[1:3], det[:, :4], im0.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
@@ -150,6 +161,7 @@ def detect(save_txt=False, save_img=False):
 
                 # Write results
                 for *xyxy, conf, _, cls in det:
+                    print('xyxy', xyxy)
                     if save_txt:  # Write to file
                         with open(save_path + '.txt', 'a') as file:
                             file.write(('%g ' * 6 + '\n') % (*xyxy, cls, conf))
@@ -194,9 +206,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='cfg file path')
     parser.add_argument('--data', type=str, default='data/coco.data', help='coco.data file path')
-    parser.add_argument('--weights', type=str, default='weights/yolov3_quant.tflite', help='path to weights file')
+    parser.add_argument('--weights', type=str, default='weights/yolov3_coco.fp32.tflite', help='path to weights file')
     parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--delegate', type=str, default='/workspace/tmp/libtensorflowlite_gpu_gl.so', help='TFLite delegate so file')  # output folder
     parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
     parser.add_argument('--nms-thres', type=float, default=0.5, help='iou threshold for non-maximum suppression')
